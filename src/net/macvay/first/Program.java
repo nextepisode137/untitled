a029178b6e4ceb4ccd093acc40b6a43f09efe71f
nextepisode137 <nextepisode137@yandex.ru>
2022-01-13 22:29:38 +0300

    added test project

---
+++ src/net/macvay/first/Program.java
@@ -0,0 +1,22 @@
+package net.macvay.first;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.*;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println(Double.POSITIVE_INFINITY);
+
+        System.out.println(DecimalFormat.getCurrencyInstance().format(100.980));
+
+        System.out.println("writed");
+    }
+
+
+}
